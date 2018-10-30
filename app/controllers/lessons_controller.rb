c36870662ca4c59075d3b28963c998ca8557f798
clarkngo <clarkngo@gmail.com>
2018-10-30 00:12:15 +0000

    require enrolled user

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,9 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course
+
   def show
+    
   end
 
   private
@@ -8,4 +12,11 @@ class LessonsController < ApplicationController
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+  def require_authorized_for_current_course
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), :alert => "You must be enrolled to view the lesson"
+    end
+  end
+
 end
