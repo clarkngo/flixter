658e4a84f926de06cd87be3687c41a136a2ded26
clarkngo <clarkngo@gmail.com>
2018-11-11 11:08:14 +0000

    modal new section

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -2,33 +2,33 @@ class Instructor::SectionsController < ApplicationController
   before_action :require_authorized_for_current_course, only: [:new, :create]
   before_action :require_authorized_for_current_section, only: [:update]
 
-  def new
-    @course = Course.find(params[:course_id])
-    @section = Section.new
-  end
-
   def create
-    @course = Course.find(params[:course_id])
-    @section = @course.sections.create(section_params)
-    redirect_to instructor_course_path(@course)
+    @section = current_course.sections.create(section_params)
+    redirect_to instructor_course_path(current_course)
   end
 
   def update
     current_section.update_attributes(section_params)
-    render plain: 'updated!'
+    render plain: 'ok!'
   end
 
   private
   
   def require_authorized_for_current_section
     if current_section.course.user != current_user
-      render plain: 'Unauthorized', status: :unauthorized
+      render plain: "Unauthorized", status: :unauthorized
     end
   end
 
   def current_section
     @current_section ||= Section.find(params[:id])
   end
+
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render plain: "Unauthorized", status: :unauthorized
+    end
+  end  
 
   helper_method :current_course
   def current_course
